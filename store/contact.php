d26186c8590e93a5a3ce1a610534bb2a1f58f70b
amitpatoliya628 <patoliyaamit628@gmail.com>
2025-09-24 12:32:37 +0530

    jhg

--- store/contact.php
+++ store/contact.php
@@ -76,27 +76,32 @@
 					</div>
 				</div>
 				<div class="col-lg-9">
-					<form class="row contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+					<form class="row contact_form" action="contact_process.php" method="post" id="contactForm">
 						<div class="col-md-6">
 							<div class="form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'">
+								<input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" required>
 							</div>
 							<div class="form-group">
-								<input type="email" class="form-control" id="email" name="email" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'">
+								<input type="email" class="form-control" id="email" name="email" placeholder="Enter email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" required>
+							</div>
+							
+							<div class="form-group">
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Enter Subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" required>
 							</div>
 							<div class="form-group">
-								<input type="text" class="form-control" id="subject" name="subject" placeholder="Enter Subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'">
-							</div>
-							<?php  if(isset($_SESSION["error"])){  ?>
-							<p class="text-danger"><?php echo $_SESSION["error"] ?></p>
 							<?php
-									unset($_SESSION["error"]);
-								}
-      						?>
+         					if(isset($_SESSION["error"])){
+          						?>
+          						<p class="login-box-msg text-danger"><?php echo $_SESSION["error"] ?></p>
+          						<?php
+          							unset($_SESSION["error"]);
+         							}
+      							?>
+							</div>
 						</div>
 						<div class="col-md-6">
 							<div class="form-group">
-								<textarea class="form-control" name="message" id="message" rows="1" placeholder="Enter Message" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'"></textarea>
+								<textarea class="form-control" name="message" id="message" rows="1" placeholder="Enter Message" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" required></textarea>
 							</div>
 						</div>
 						<div class="col-md-12 text-right">
--- store/contact_process.php
+++ store/contact_process.php
@@ -1,39 +1,26 @@
 <?php
+session_start();
+if(isset($_SESSION['web_uname'])){
+    include_once('config.php');
+    extract($_POST);
+    $filename = time()."_".$_FILES['image']['name'];
+    $path="../images_/categories/".$filename;
+    $catdescription=mysqli_real_escape_string($conn,$catdescription);
 
-    $to = "patoliyaamit628@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $cmessage = $_REQUEST['message'];
+    if(move_uploaded_file($_FILES['image']['tmp_name'],$path)){
+        $qry = "insert into categories (catname,catdescription,image) values('".$catname."','".$catdescription."','".$filename."')";
+        mysqli_query($conn, $qry) or exit("category insert fail".mysqli_error($conn));
+        $_SESSION['error']="category added successfully";
+        header("location:category.php");
+    }
+    else{
+        $_SESSION["error"] = "file upoad fail";
+        header("location:category.php");
+    }
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
-
-    $logo = 'img/logo.png';
-    $link = '#';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-    $send = mail($to, $subject, $body, $headers);
-
-	$_SESSION["error"] = "your mail sent soon.";
-	header("location:contact.php");
-
+}else{
+  $_SESSION["error"] = "you are not authorize to access this page without login";
+  header("location:index.php");
+}
 ?>
