a0ba5dc732c922dfb24616d3cec42c2c468cddc7
amitpatoliya628 <patoliyaamit628@gmail.com>
2025-10-07 22:57:11 +0530

    ./.././././././/.

--- admin/user_add.php
+++ admin/user_add.php
@@ -12,17 +12,17 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini ">
 <div class="wrapper">
     <!-- Navbar -->
     <?php
-        include_once("includes/header.php");
+      include_once("includes/header.php");
     ?>
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
     <?php 
-        include_once("includes/sidebar.php");
+      include_once("includes/sidebar.php");
     ?>
 
   <!-- Content Wrapper. Contains page content -->
@@ -54,59 +54,65 @@
             <!-- Horizontal Form -->
             <div class="card card-info">
                 <div class="card-header">
-                    <h3 class="card-title">User Form</h3>
+                  <h3 class="card-title">User Form</h3>
                 </div>
-                <!-- /.card-header -->
                 <!-- form start -->
-                <form class="form-horizontal">
+                <form class="form-horizontal" action="user_add_db.php" method="post" id="contactForm">
                     <div class="card-body">
                         <div class="form-group row">
                             <span class="col-sm-1"></span>
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Full Name : </label>
                             <div class="col-sm-8">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                <input type="text" required class="form-control" id="fullname" name="username" placeholder="Enter Full Name" onfocus="this.placeholder = ''" >
                             </div>
                         </div>
                         <div class="form-group row">
                             <span class="col-sm-1"></span>
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Email : </label>
                             <div class="col-sm-8">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                <input type="email" required class="form-control" id="email" name="email_" placeholder="Enter Email" onfocus="this.placeholder = ''">
                             </div>
                         </div>
                         <div class="form-group row">
                             <span class="col-sm-1"></span>
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Mobile No : </label>
                             <div class="col-sm-8">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                <input type="tel" required class="form-control" id="mobile_no" name="mobile_no" placeholder="Enter Mobile No" pattern="[0-9]{10}" maxlength="10" oninput="if(this.value.length > 10) this.value = this.value.slice(0, 10);" onfocus="this.placeholder=''">
                             </div>
                         </div>
                         <div class="form-group row">
                             <span class="col-sm-1"></span>
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Birth Date : </label>
                             <div class="col-sm-8">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                <input type="date" required class="form-control" id="bdate" name="bdate">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <span class="col-sm-1"></span>
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">User Role : </label>
+                            <div class="col-sm-8">
+                                <input type="text" required class="form-control" id="fullname" name="role" placeholder="Enter User's Role" onfocus="this.placeholder = ''" >
                             </div>
                         </div>
                         <div class="form-group row">
                             <span class="col-sm-1"></span>
                             <label for="inputPassword3" class="col-sm-2 col-form-label">Password : </label>
                             <div class="col-sm-8">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                              <input type="password" required class="form-control" id="password" name="password" placeholder="Enter Password" onfocus="this.placeholder = ''">
                             </div>
                         </div>
                         <div class="form-group row">
                             <span class="col-sm-1"></span>
                             <label for="inputPassword3" class="col-sm-2 col-form-label">Conform Password : </label>
                             <div class="col-sm-8">
-                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                              <input type="password" required class="form-control" id="confirm_password" name="confirm_password" placeholder="Confirm Password" onfocus="this.placeholder = ''">
                             </div>
                         </div>
                     </div>
                     <!-- /.card-body -->
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-info">Sign in</button>
-                        <button type="submit" class="btn btn-default float-right">Cancel</button>
+                        <button type="submit" class="btn btn-info">Create</button>
+                        <a href="users.php" ><button type="button" class="btn btn-default float-right">Cancel</button></a>
                     </div>
                     <!-- /.card-footer -->
                 </form>
@@ -128,6 +134,17 @@
       include_once("includes/script.php");
     ?>
 
+    <script>
+		  document.getElementById("contactForm").addEventListener("submit", function(e) {
+      	let password = document.getElementById("password").value;
+      	let confirmPassword = document.getElementById("confirm_password").value;
+
+      	if (password !== confirmPassword) {
+          	e.preventDefault(); 
+          	alert("Password and Conform Password is not matching!");
+      	}
+		  });
+	  </script>
     <script>
         $(function () {
         bsCustomFileInput.init();
